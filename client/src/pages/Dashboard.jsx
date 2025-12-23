f18af4d4c61ddc8359146a0c64f4b02a7cc05318
Ilma Makarevic <makarevic85@gmail.com>
2025-12-23 21:34:56 +0100

    Added TV shows to dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,19 +1,21 @@
 import './Dashboard.css';
 import MovieRow from '../components/MovieRow';
 
-
 const Dashboard = () => {
     const BASE_URL = "http://localhost:5081/api/movies";
+    const TV_SHOWS_URL = "http://localhost:5081/api/TvShows";
 
     return(
 
         <div className='dashboard-container'>
         
             <MovieRow title="Popular Movies" url={`${BASE_URL}/popular`} />
+            
+            <MovieRow 
+                title="Popular TV Shows" 
+                url={`${TV_SHOWS_URL}/popular`} 
+            />
 
-            {/* Ovdje možeš dodati ostale sekcije kada napraviš rute u .NET-u */}
-
-            {/*<MovieRow title="Trending Movies" url={`${BASE_URL}/trending`} />*/}
         </div>
 
     );
